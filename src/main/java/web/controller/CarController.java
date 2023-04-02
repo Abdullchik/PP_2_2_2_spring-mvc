cb267ba81dd562602e3f196382af49bc3311d697
Alex052524 <abdul_akhmedov_2004@mail.ru>
2023-04-02 11:53:25 +0300

    end

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 
 import java.util.List;
@@ -11,8 +12,8 @@ import java.util.List;
 public class CarController {
 
     @GetMapping(value = "/car")
-    public String printCar(ModelMap model) {
-        List<Car> carList = Car.carList();
+    public String printCars( @RequestParam(value = "count", required = false) int count, ModelMap model) {
+        List<Car> carList = Car.carList(count);
         model.addAttribute("carList", carList);
         return "cars";
     }
--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.ToString;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Getter
 @AllArgsConstructor
@@ -18,13 +19,13 @@ public class Car {
 
     private String color;
 
-    public static List<Car> carList() {
-        return List.of(
+    public static List<Car> carList(int count) {
+        return Stream.of(
                 new Car(1, "lada", "white"),
                 new Car(2, "ferrari", "red"),
                 new Car(3, "audi", "black"),
                 new Car(4, "kia", "yellow"),
                 new Car(5, "BMW", "blue")
-        );
+        ).limit(count >= 5 ? Integer.MAX_VALUE : count).toList();
     }
 }
